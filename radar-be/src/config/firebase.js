73b7450ca99a0e3bf9f799c46a01841cc65dd000
DiMinch <phanduyminh1262@gmail.com>
2025-05-27 14:21:48 +0700

    fix: db route

--- radar-be/src/config/firebase.js
+++ radar-be/src/config/firebase.js
@@ -1,5 +1,6 @@
+const path = require("path");
 const admin = require("firebase-admin");
-const serviceAccount = require("../../serviceAccountKey.json");
+const serviceAccount = require(path.join(process.cwd(), "serviceAccountKey.json"));
 
 admin.initializeApp({
   credential: admin.credential.cert(serviceAccount),
